91c2a601b9747db703c49fbd7a4b4a1c0c664968
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2023-12-22 15:35:42 +0100

    add solutions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import React from "react";
-import "./styles.css";
+import React from 'react';
+
+function Sum({ valueA, valueB }) {
+  const sum = valueA + valueB;
+  return <h1>{valueA} + {valueB} = {sum}</h1>;
+}
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div className="App">
+      <Sum valueA={5} valueB={3} />
+    </div>
+  );
 }
